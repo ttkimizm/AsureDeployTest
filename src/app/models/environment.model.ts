c0c6c220c58fb8357a79450dd5cb2fd44ea23f8f
ttkimizm <abc922476@outlook.jp>
2025-05-30 20:41:29 +0900

    Delete src directory

--- src/app/models/environment.model.ts
+++
@@ -1,9 +0,0 @@
-// Azure AD 認証情報格納用インターフェース
-export interface Environment {
-    production?: boolean;
-    baseurl?: string;
-    azure_graph_url?: string;
-    azure_clientid: string;
-    azure_authority?: string;
-    azure_redirecturi: string;
-}
